396b5fb35a55bfb9e07a2ceca6018bc2b5f44501
Ruslan <r1554899@gmail.com>
2024-07-13 18:15:20 -0700

    Add Metadata + Opengraph Image

--- app/layout.js
+++ app/layout.js
@@ -8,22 +8,22 @@ const inter = Inter({ subsets: ["latin"] });
 
 // This is the metadata for the website. Change the values to match your website. Upload the openGraph.png to your public folder
 export const metadata = {
-  metadataBase: new URL('https://www.foym.org/'),
-  title: " - An FOYM Template",
-  description: "",
+  metadataBase: new URL('https://learn.foym.org/'),
+  title: "Learn Link - An FOYM Template",
+  description: "This is a template for organizations modeled after a made-up tutoring nonprofit called “Learn Link.” It is entirely free to use and customize. Created by FOYM, a project by Ruslan Mukhamedvaleev, under the MIT license.",
   openGraph: {
-    title: " - An FOYM Template",
-    description: "",
-    url: '',
-    siteName: ' - An FOYM Template',
-    // images: [
-    //   {
-    //     url: '/openGraph.png',
-    //     width: 1600,
-    //     height: 900,
-    //     alt: 'An image on the the website\s name "Template - FOYM" and star like characters in the background pointing at the name',
-    //   },
-    // ], 
+    title: "Learn Link - An FOYM Template",
+    description: "This is a template for organizations modeled after a made-up tutoring nonprofit called “Learn Link.” It is entirely free to use and customize. Created by FOYM, a project by Ruslan Mukhamedvaleev, under the MIT license.",
+    url: 'https://learn.foym.org/',
+    siteName: 'Learn Link - An FOYM Template',
+    images: [
+      {
+        url: '/openGraph.png',
+        width: 1600,
+        height: 900,
+        alt: 'An image of the website\s name "Learn Link - An FOYM Template" and star like characters in the background pointing at the name',
+      },
+    ], 
     locale: 'en_US',
     type: 'website',
   },
